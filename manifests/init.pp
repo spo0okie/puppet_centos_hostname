76b1669baab548ab5249356a0c085ae039ad2d63
root <root@main.reviakin.net>
2021-02-06 11:52:30 +0300

    added check for OS environment

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,7 +1,11 @@
 class centos_hostname (
 	$hname='localhost'
 ) {
-	host {$hname:ip =>'127.0.0.1',host_aliases=>['localhost','localhost.localdomain','localhost4','localhost4.localdomain4']}
-	host {'localhost':ip =>'127.0.0.1',ensure=>'absent'}
-	host {'localhost6':ip =>'::1',host_aliases=>['localhost','localhost.localdomain','localhost6.localdomain6']}
+	case $::operatingsystem {
+		'CentOS': {
+			host {$hname:ip =>'127.0.0.1',host_aliases=>['localhost','localhost.localdomain','localhost4','localhost4.localdomain4']}
+			host {'localhost':ip =>'127.0.0.1',ensure=>'absent'}
+			host {'localhost6':ip =>'::1',host_aliases=>['localhost','localhost.localdomain','localhost6.localdomain6']}
+		}
+	}
 }
